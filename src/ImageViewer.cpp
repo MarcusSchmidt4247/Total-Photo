52f918947ac68598b6baa9207e0592e5b1b37b46
Marcus Schmidt <schmidt.marcust@gmail.com>
2022-11-05 10:08:21 -0700

    Loads and displays images, key presses to view, and window uses wxSplitterWindow

--- src/ImageViewer.cpp
+++ src/ImageViewer.cpp
@@ -4,11 +4,11 @@
 #include "ImageViewer.h"
 #include <wx/menu.h>
 #include <wx/panel.h>
-#include <wx/sizer.h>
 #include <wx/stattext.h>
 #include <wx/checkbox.h>
-#include <sys/stat.h>
-#include <time.h>
+#include <wx/image.h>
+#include <wx/bitmap.h>
+#include <wx/splitter.h>
 #include <iostream>
 
 ImageViewer::ImageViewer(wxWindow *parent, wxWindowID id, const wxString &title, std::string path)
@@ -41,11 +41,26 @@ ImageViewer::ImageViewer(wxWindow *parent, wxWindowID id, const wxString &title,
 	menuBar->Append(viewerMenu, "Image Viewer");
 	this->SetMenuBar(menuBar);
 
+	//*************************
+	// Create splitter window *
+	//*************************
+
+	wxSplitterWindow *splitter = new wxSplitterWindow(this, wxID_ANY, wxDefaultPosition, wxDefaultSize, wxSP_THIN_SASH);
+	splitter->SetSashGravity(0);
+	splitter->SetMinimumPaneSize(50);
+	splitter->Bind(wxEVT_KEY_DOWN, &ImageViewer::OnKeyPress, this);
+
+	wxBoxSizer *topSizer = new wxBoxSizer(wxHORIZONTAL);
+	topSizer->Add(splitter, wxSizerFlags(1).Expand());
+
+	wxPanel *controlPanel = new wxPanel(splitter, wxID_ANY, wxDefaultPosition, wxDefaultSize, wxBORDER_SIMPLE);
+	wxBoxSizer *controlSizer = new wxBoxSizer(wxVERTICAL);
+
 	//*********************************
 	// Create directory control panel *
 	//*********************************
 
-	wxPanel *directoryPanel = new wxPanel(this, wxID_ANY, wxDefaultPosition, wxDefaultSize,
+	wxPanel *directoryPanel = new wxPanel(controlPanel, wxID_ANY, wxDefaultPosition, wxDefaultSize,
 										  wxTAB_TRAVERSAL | wxBORDER_SIMPLE);
 	wxBoxSizer *directorySizer = new wxBoxSizer(wxVERTICAL);
 
@@ -72,11 +87,12 @@ ImageViewer::ImageViewer(wxWindow *parent, wxWindowID id, const wxString &title,
 	}
 
 	directoryPanel->SetSizer(directorySizer);
+	controlSizer->Add(directoryPanel, wxSizerFlags(1).Expand());
 
 	//**********************************
 	// Create file types control panel *
 	//**********************************
-	wxPanel *typesPanel = new wxPanel(this, wxID_ANY, wxDefaultPosition, wxDefaultSize,
+	wxPanel *typesPanel = new wxPanel(controlPanel, wxID_ANY, wxDefaultPosition, wxDefaultSize,
 									  wxTAB_TRAVERSAL | wxBORDER_SIMPLE);
 	wxBoxSizer *typesSizer = new wxBoxSizer(wxVERTICAL);
 
@@ -100,29 +116,29 @@ ImageViewer::ImageViewer(wxWindow *parent, wxWindowID id, const wxString &title,
 	}
 	
 	typesPanel->SetSizer(typesSizer);
+	controlSizer->Add(typesPanel, wxSizerFlags().Expand());
 
 	//************************************************
 	// Create the image viewer section of the window *
 	//************************************************
 
-	wxBoxSizer *imageSizer = new wxBoxSizer(wxHORIZONTAL);
-	imageSizer->Add(new wxStaticText(this, wxID_ANY, "Placeholder", wxDefaultPosition, wxDefaultSize, wxALIGN_CENTRE_HORIZONTAL), wxSizerFlags(1).Center());
+	wxPanel *imagePanel = new wxPanel(splitter, wxID_ANY, wxDefaultPosition, wxDefaultSize, wxBORDER_NONE);
+	imageSizer = new wxBoxSizer(wxHORIZONTAL);
+	imageBitmap = new wxStaticBitmap(imagePanel, wxID_ANY, wxBitmap(1,1));
+	imageBitmap->SetScaleMode(wxStaticBitmap::Scale_AspectFit);
+	imageSizer->Add(imageBitmap, wxSizerFlags(1).Expand());
 	imageSizer->SetMinSize(700, 500);
+	imagePanel->SetSizer(imageSizer);
 
 	//****************************
 	// Create the overall window *
 	//****************************
 
-	wxBoxSizer *topSizer = new wxBoxSizer(wxHORIZONTAL);
-
-	wxBoxSizer *controlsSizer = new wxBoxSizer(wxVERTICAL);
-	controlsSizer->Add(directoryPanel, wxSizerFlags(1).Expand());
-	controlsSizer->Add(typesPanel, wxSizerFlags().Expand());
-
-	topSizer->Add(controlsSizer, wxSizerFlags().Expand());
-	topSizer->Add(imageSizer, wxSizerFlags(1).Expand());
+	controlPanel->SetSizer(controlSizer);
+	splitter->SplitVertically(controlPanel, imagePanel);
 	this->SetSizerAndFit(topSizer);
-	this->Fit();
+
+	splitter->SetSashPosition(controlSizer->GetMinSize().GetWidth());
 }
 
 void ImageViewer::OnRefresh(wxCommandEvent &event)
@@ -151,6 +167,54 @@ void ImageViewer::OnFileTypeToggled(wxCommandEvent &event)
 	fileTypes[index].active = !fileTypes[index].active;
 }
 
+void ImageViewer::OnKeyPress(wxKeyEvent &event)
+{
+	if (files.size() > 0)
+	{
+		int keyCode = event.GetKeyCode();
+		if (keyCode == WXK_RIGHT)
+		{
+			imageIndex++;
+			if (imageIndex >= files.size())
+				imageIndex = 0;
+
+			LoadFile(files[imageIndex].path + files[imageIndex].name);
+		}
+		else if (keyCode == WXK_LEFT)
+		{
+			imageIndex--;
+			if (imageIndex < 0)
+				imageIndex = files.size() - 1;
+
+			LoadFile(files[imageIndex].path + files[imageIndex].name);
+		}
+	}
+
+	event.Skip();
+}
+
+void ImageViewer::LoadFile(std::string path)
+{
+	static bool first = true;
+	static wxImage image;
+
+	if (first)
+	{
+		image.AddHandler(new wxPNGHandler);
+		image.AddHandler(new wxJPEGHandler);
+		first = false;
+	}
+
+	if (image.LoadFile(path))
+	{
+		imageBitmap->SetBitmap(wxBitmap(image));
+		imageSizer->Layout();
+		image.Destroy();
+	}
+	else
+		std::cout << "Failed to load file \"" << path << "\"" << std::endl;
+}
+
 void ImageViewer::GetImages()
 {
 	files.clear();
@@ -176,38 +240,43 @@ void ImageViewer::GetImages()
 						{
 							if (type.active && extension.compare(type.value) == 0)
 							{
-								// Obtain the relative path to this file from the root directory
-								int rootLength = rootPath.string().length() + 1; // +1 accounts for the /
-								std::string relativePath = entry.path().string().substr(rootLength, entry.path().string().length() - rootLength);
+								std::string name = entry.path().filename().string();
+								std::string path = entry.path().string().substr(0, entry.path().string().length() - name.length());
 
-								files.push_back(relativePath);
+								// Struct documentation: https://man7.org/linux/man-pages/man0/sys_stat.h.0p.html
+								struct stat info;
+								time_t time;
+								// Function documentation: https://pubs.opengroup.org/onlinepubs/007908799/xsh/stat.html
+								if (stat(entry.path().c_str(), &info) == 0)
+								{
+									time = info.st_mtime;
+								}
+								else
+								{
+									std::cout << "Unable to obtain time for \"" << path << "\"" << std::endl;
+									time = 0;
+								}
+
+								File file = { name, path, time };
+								
+								//************************************************
+								// Insert into vector according to sorting rules *
+								//************************************************
+								files.push_back(file);
 							}
 						}
 					}
 				}
-
-				/*
-				// Evaluate each file according to the sort method
-				if (sortMethod == ID_SORT_DATE)
-				{
-					// Struct documentation: https://man7.org/linux/man-pages/man0/sys_stat.h.0p.html
-					struct stat info;
-					// Function documentation: https://pubs.opengroup.org/onlinepubs/007908799/xsh/stat.html
-					if (stat("name.extension", &info) == 0)
-					{
-						struct tm *timeInfo = localtime(&info.st_mtime);
-						// asctime(timeInfo)
-					}
-					else
-					{
-						// Error!
-					}
-				}
-				*/
 			}
 			else
 				std::cout << "Directory \"" << path.string() << "\" does not exist" << std::endl;
 		}
+	}
+
+	if (files.size() > 0)
+	{
+		imageIndex = 0;
+		LoadFile(files[imageIndex].path + files[imageIndex].name);
 	}
 }
 
@@ -240,10 +309,12 @@ void ImageViewer::PrintActive(std::vector<ImageViewer::ToggledString> vector, st
 	std::cout << std::endl;
 }
 
-// To draw images (official sample, relevant classes):
-// https://github.com/wxWidgets/wxWidgets/blob/master/samples/image/canvas.cpp
-// and
-// https://github.com/wxWidgets/wxWidgets/blob/master/samples/image/image.cpp
+void ImageViewer::PrintFile(ImageViewer::File file)
+{
+	std::cout << "\"" << file.name << "\" file:" << std::endl;
+	std::cout << "-- Path: " << file.path << std::endl;
+	std::cout << "-- Modified: " << asctime(localtime(&file.modifiedTime)) << std::endl << std::endl;
+}
 
 // Don't catch wxID_NEW because the event will rise to TotalPhoto.cpp
 BEGIN_EVENT_TABLE(ImageViewer, wxFrame)
--- src/ImageViewer.h
+++ src/ImageViewer.h
@@ -9,6 +9,9 @@
 #include <string>
 #include <filesystem>
 #include <vector>
+#include <sys/stat.h>
+#include <wx/statbmp.h>
+#include <wx/sizer.h>
 
 class ImageViewer : public wxFrame
 {
@@ -17,6 +20,13 @@ public:
 	{
 		std::string value;
 		bool active = false;
+	};
+
+	struct File
+	{
+		std::string name;
+		std::string path;
+		time_t modifiedTime;
 	};
 
 	ImageViewer(wxWindow *parent, wxWindowID id, const wxString &title, std::string path);
@@ -29,25 +39,35 @@ private:
 	void OnSortChanged(wxCommandEvent &event);
 	void OnDirectoryToggled(wxCommandEvent &event);
 	void OnFileTypeToggled(wxCommandEvent &event);
+	void OnKeyPress(wxKeyEvent &event);
 
+	void LoadFile(std::string path);
+
+	// Getters
 	void GetImages();
 	int GetId(int type, int index);
 	int GetIndex(int type, int id);
+
+	// Print functions
 	void PrintActive(std::vector<ToggledString> vector, std::string name);
+	void PrintFile(File file);
 
 	// Constant variables
 	static const int ID_SORT_NAME = wxID_HIGHEST + 1;
 	static const int ID_SORT_DATE = wxID_HIGHEST + 2;
 	static const int ID_SORT_RANDOM = wxID_HIGHEST + 3;
 
-	// Settings variables
-	std::filesystem::path rootPath;
-	int sortMethod = ID_SORT_NAME;
-
-	// Lists of active directories and file types, and sorted files
+	// Vector variables
 	std::vector<ToggledString> directories;
 	std::vector<ToggledString> fileTypes;
-	std::vector<std::string> files;
+	std::vector<File> files;
+
+	// Miscellaneous variables
+	wxStaticBitmap *imageBitmap;
+	wxBoxSizer *imageSizer;
+	std::filesystem::path rootPath;
+	int sortMethod = ID_SORT_NAME;
+	int imageIndex = 0;
 };
 
 #endif
